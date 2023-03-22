4cb7399fee9821e3435281d0db6f0194c5112eae
devdashk <devdashk66@gmail.com>
2023-03-22 21:24:22 +0600

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
-import Footer from "../components/header/Footer";
-import Header from "../components/header/header";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function App({ Component, pageProps }) {
   return (
